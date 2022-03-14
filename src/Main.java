b0a362d3b5f0cf2847cb084b030f223a6bef7746
MateuszMal <mmateuszecki@gmail.com>
2022-03-14 23:28:17 +0100

    Added javafx configuration

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
 import files.ExtractFiles;
 import files.ReadSgmFile;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
+        launch();
         ReadSgmFile f = new ReadSgmFile();
         List<List<String>> files = f.readFiles();
 
